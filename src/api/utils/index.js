a3a594865f64537de0986bdbcd83890648bfc27a
Igor Kruhliak <igor.kruhliak@nixsolutions.com>
2019-08-13 10:20:08 +0300

    Added info to readme

--- src/api/utils/index.js
+++ src/api/utils/index.js
@@ -2,5 +2,13 @@ export const getURLWithKey = () => {
   const url = process.env.REACT_APP_API_URL;
   const key = process.env.REACT_APP_API_KEY;
 
+  if (!url) {
+    console.log('[URL] Undefined');
+  }
+
+  if (!key) {
+    console.log('[API_KEY] Undefined');
+  }
+
   return `${ url }?key=${ key }`;
 };
